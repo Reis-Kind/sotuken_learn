d3d67ab8316f9068ee3f47f1f78f1862798cdc86
Reis-Kind <chikoutamu9@gmail.com>
2026-08-31 16:34:33 +0900

    a

--- script/binarize_ga.py
+++ script/binarize_ga.py
@@ -245,7 +245,7 @@ def main():
 
     plt.tight_layout()
     plt.savefig("./output/binarize_ga.png")
-    print("グラフを ./output/binarize_ga.png に保存した．")
+    print("グラフを ./output/binarize_ga_BN.png に保存した．")
 
 
 if __name__ == "__main__":
