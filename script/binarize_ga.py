341668e5d6ca3af21c22a4dc6def11a5be82dab1
Reis-Kind <chikoutamu9@gmail.com>
2026-09-02 14:27:27 +0900

    BN一旦削除

--- script/binarize_ga.py
+++ script/binarize_ga.py
@@ -7,15 +7,12 @@ from torchvision import datasets, transforms
 class BinarizedNeuroEvo(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(784, 128, bias=False)
-        # 移動平均は無視
-        self.bn1 = nn.BatchNorm1d(128)
+        self.fc1 = nn.Linear(784, 128, bias=True)
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = x.view(-1, 784)
         x = self.fc1(x)
-        x = self.bn1(x)
         x = torch.where(x >= 0, 1.0, -1.0)
         # 出力層は二値化しない
         x = self.fc2(x)
@@ -23,7 +20,7 @@ class BinarizedNeuroEvo(nn.Module):
 
 
 def evaluate(model, w, b, x, y):
-    """
+    """a
     個体の適応度を計算する関数
 
     """
@@ -33,11 +30,11 @@ def evaluate(model, w, b, x, y):
     model.fc1.weight.data = w[:w_size].view(128, 784).clone()
     model.fc2.weight.data = w[w_size:].view(10, 128).clone()
 
-    model.bn1.weight.data = b[:128].clone()        
-    model.bn1.bias.data   = b[128:256].clone()    
-    model.fc2.bias.data   = b[256:].clone()        # fc2.bias
+    model.fc1.bias.data = b[:128].clone()        
+    model.fc2.bias.data = b[128:].clone()        # fc2.bias
 
-    model.train()
+    # BNがなくなったのでtrain/evalの区別は不要、evalで統一
+    model.eval()
     with torch.no_grad():
         outputs = model(x)
         loss = nn.functional.cross_entropy(outputs, y)
@@ -85,7 +82,7 @@ def genetic_algorithm(model, x_eval, y_eval):
     np.random.seed(42)
 
     weight = 128 * 784 + 10 * 128
-    bias = 128 + 128 + 10
+    bias = 128 + 10
 
     # 島の数 * 島あたりの個体数 * 重みの総数の乱数を生成し、二値化
     island_w = torch.where(
@@ -220,9 +217,8 @@ def main():
     w1_size = 128 * 784
     model.fc1.weight.data = best_w[:w1_size].view(128, 784)
     model.fc2.weight.data = best_w[w1_size:].view(10, 128)
-    model.bn1.weight.data = best_b[:128].clone()  
-    model.bn1.bias.data = best_b[128:256].clone()  
-    model.fc2.bias.data = best_b[256:].clone()  
+    model.fc1.bias.data = best_b[:128].clone()    
+    model.fc2.bias.data = best_b[128:].clone()  
 
     # 学習曲線の描画
     plt.figure(figsize=(10, 4))
@@ -242,7 +238,7 @@ def main():
 
     plt.tight_layout()
     plt.savefig("./output/binarize_ga.png")
-    print("グラフを ./output/binarize_ga_BN.png に保存した．")
+    print("グラフを ./output/binarize_ga_noBN.png に保存した．")
 
 
 if __name__ == "__main__":
