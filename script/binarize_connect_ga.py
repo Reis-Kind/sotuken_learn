b9f3ad591dcf673f3b0e16439d68b2dd0fa249f3
REIS-KIND <chikoutamu9@gmail.com>
2026-09-07 14:23:30 +0900

    バイナリコネクトとGA合体！！！

--- script/binarize_connect_ga.py
+++ script/binarize_connect_ga.py
@@ -16,4 +16,24 @@ class BainarizeSTE(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         return grad_output
+
+
+class BinarizedNeuroEvo(nn.Module):
+    """
+    
+    
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 64, bias=True)
+        self.fc2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 784)
+        x = self.fc1(x)
+        x = torch.where(x >= 0, 1.0, -1.0)
+        # 出力層は二値化しない
+        x = self.fc2(x)
+        return x
     
